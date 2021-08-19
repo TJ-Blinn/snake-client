845fc40685bd427627ab64cf262467ee507b2de5
TJ-Blinn <tjblinn@hotmail.com>
2021-08-19 01:11:07 +0000

    added connect module, event handler, and name command to client.js for SNEK

---
+++ client.js
@@ -0,0 +1,42 @@
+/*
+client module handle a TCP connection
+*/
+
+// * Establishes connection with the game server
+const net = require("net");
+//const { IP, PORT } = require("./play")
+
+/*  // ES6 object shorthand to export an object containing our connect function.
+
+// const host = "localhost";
+// const port = 50541;
+// const obj = { host, port };
+// const conn2 = net.createConnection(obj);
+
+*/
+
+// Establishes connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // register event handler for connect Event. Triggered upon connection
+  conn.on("connect", () => {
+    console.log("connected to server ----------");
+    conn.write("Successfully connected to game server");
+    conn.write("Name: TEJ");
+  });
+
+  // Handling Data. receive this welcome greeting (data) and, for now, simply print it out to the screen
+  conn.on("data", (data) => {
+    console.log("Server says: _______", data);
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -2,26 +2,8 @@
 // IP address and port information: 10.0.0.209 PORT 50541
 // Snek-multiplayer IP: 10.0.2.15 PORT: 50541
 
-const net = require("net");
-
-/**
- * Establishes connection with the game server
- */
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // Handling Data. receive this welcome greeting (data) and, for now, simply print it out to the screen
-  conn.on("data", (data) => {
-    console.log("Server says: you ded cuz you idled ", data);
-  });
-
-  return conn;
-};
+const { connect } = require("./client");
+// const connect = require("./client");
 
 console.log("Connecting ...");
 connect();
